d31cbab68ef2353f6fe2d90eea6c75204390d8b5
Prathap_Paapi <myselfprathap@gmail.com>
2022-10-07 21:36:42 +0530

    made correction in main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,20 +2,17 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A obj1 = new B();
-        System.out.println(obj1.meth());
-
         B obj2 = new B();
-        System.out.println(obj2.meth());
+        obj2.meth();
     }
 
-    static class A{
+    public static class A{
         public static String meth(){
             return "Invoking method from class A";
         }
     }
 
-    static class B extends A{
+    public static class B extends A{
         public static String meth(){
             return "Method is overridden in Extendend class B";
         }
